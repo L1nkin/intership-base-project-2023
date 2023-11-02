66176c4035f818f4c4399daa7687d1076f7d4662
Valentin Panasyuk <v.panasuk@appkode.ru>
2023-11-02 14:05:52 +0300

    refactor(function): remove unused function

--- src/features/writing-sum-form/lib.ts
+++ src/features/writing-sum-form/lib.ts
@@ -1,9 +1,3 @@
-const rublesSymbol = '₽'
-
-export const configRublesString = (value: number): string => {
-    return `${value}` + rublesSymbol
-}
-
 export const convertToNumber = (value: string): number => {
     return Number(value.replace(/[^0-9.,]/g, ''))
 }
